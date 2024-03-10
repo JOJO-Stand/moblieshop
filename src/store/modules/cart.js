4c63da8b6477fade94cb25c4329ec6b8a9a90489
JOJO <Xuezhou.Gu@Outlook.com>
2024-03-10 15:34:02 +0800

    无法取消全选

---
+++ src/store/modules/cart.js
@@ -0,0 +1,67 @@
+import { getCartList } from '@/api/cart'
+
+export default {
+  namespaced: true,
+  state () {
+    return {
+      cartList: []
+    }
+  },
+  mutations: { // 仅支持同步
+    // 提供一个设置cartList的 mutation
+    setCartList (state, newList) {
+      state.cartList = newList
+    },
+    toggleCheck (state, goodsId) {
+      // 让对应的ID的项 状态取反
+      const goods = state.cartList.find(item => item.goods_id === goodsId)
+      goods.isChecked = !goods.isChecked
+    },
+    toggleAllCheck (state, flag) {
+      // 让所有的小选框同步状态
+      state.cartList.forEach(item => {
+        item.isChecked = flag
+      })
+    }
+  },
+  actions: { // 支持异步
+    async getCartAction (context) {
+      const { data } = await getCartList()
+      // 后台返回的数据中，不包含复选框的选中状态，为了实现将来的功能
+      // 需要手动维护数据，给每一项，添加一个isChecked状态（标记当前商品是否选中）
+      data.list.forEach(item => {
+        item.isChecked = true
+      })
+
+      context.commit('setCartList', data.list)
+    }
+  },
+  getters: {
+    // 求所有的商品累计总数
+    cartTotal (state) {
+      return state.cartList.reduce((sum, item) => sum + item.goods_num, 0)
+    },
+
+    // 选中的商品
+    selCartList (state) {
+      return state.cartList.filter(item => item.isChecked)
+    },
+
+    // 选中的商品总数
+    selCount (state, getters) {
+      return getters.selCartList.reduce((sum, item) => sum + item.goods_num, 0)
+    },
+
+    // 选中的总价
+    selPrice (state, getters) {
+      return getters.selCartList.reduce((sum, item) => {
+        return sum + item.goods_num * item.goods.goods_price_min
+      }, 0).toFixed(2)
+    },
+
+    // 是否全选
+    isAllChecked (state) {
+      return state.cartList.every(item => item.isChecked)
+    }
+  }
+}
--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -3,8 +3,10 @@ import 'vant/lib/index.css'
 // 插件安装初始化：内部会将所有的vant组件进行导入注册
 Vue.use(Vant) */
 import Vue from 'vue'
-import { Icon, Search, Swipe, SwipeItem, Grid, GridItem, Button, Switch, Rate, Tabbar, TabbarItem, NavBar, Toast, ActionSheet, Dialog } from 'vant'
+import { Icon, Search, Swipe, SwipeItem, Grid, GridItem, Button, Switch, Rate, Tabbar, TabbarItem, NavBar, Toast, ActionSheet, Dialog, Checkbox, CheckboxGroup } from 'vant'
 
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
 Vue.use(Dialog)
 Vue.use(ActionSheet)
 Vue.use(Icon)
